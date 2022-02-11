939700569fda931570b9e3074fcb951137e7098e
Newtexas <89256338+Newtexas28@users.noreply.github.com>
2022-02-11 23:53:02 +0100

    using a singel function

--- Input del 3/js/main.js
+++ Input del 3/js/main.js
@@ -3,41 +3,6 @@
  */
 
 /*
- * Making a function plus to add the two input values together,
- * add returning the value.
- */
-
-const plus = (input1, input2) => {
-    return input1 + input2; 
-};
-
-/*
- * Making a function minus to subtract the two input values from each other,
- * in this case subtracting input2 from input1 and returing the value.
- */
-
-const minus = (input1, input2) => {
-    return input1 - input2;
-};
-
-/*
- * Making a function multiplication to multipli input1 and input2
- * the returning the value.  
- */
-const multiplication = (input1, input2) => {
-    return input1 * input2;
-};
-
-/*
- * Making a function to divide the numbers from input1 with the numbers from input2,
- * then returning the value.
- */
-
-const divisijon = (input1, input2) => {
-    return input1/input2;
-};
-
-/*
  * Taking user inputs from the user on the websit and declaring it in the variabels,
  * input1 and input2. 
  * These varibals is used in the four functions above plus(), mius(), mulitplication()
@@ -49,6 +14,40 @@ const divisijon = (input1, input2) => {
 const output = () => {
     const input1 = parseInt(document.getElementById('number1').value);
     const input2 = parseInt(document.getElementById('number2').value);
-    if (pluss) {}   
+    
+    /*
+    * Making a function plus to add the two input values together,
+    * add returning the value.
+    */
 
-}
+    const plus = (input1, input2) => {
+        return input1 + input2; 
+    };
+
+    /*
+     * Making a function minus to subtract the two input values from each other,
+     * in this case subtracting input2 from input1 and returing the value.
+     */
+
+    const minus = (input1, input2) => {
+        return input1 - input2;
+    };
+
+    /*
+     * Making a function multiplication to multipli input1 and input2
+     * the returning the value.  
+     */
+    const multiplication = (input1, input2) => {
+        return input1 * input2;
+    };
+
+    /*
+     * Making a function to divide the numbers from input1 with the numbers from input2,
+     * then returning the value.
+     */
+
+    const divisijon = (input1, input2) => {
+        return input1/input2;
+    };   
+
+};
