a3b8e1598cd55d70131b6ca694940fb71ba6c79d
Jon Håkon Lia <jh-lia@online.no>
2022-02-11 13:32:29 +0100

    Adding coments and code toLøkker

--- Løkker - Småoppgåver 1/js/main.js
+++ Løkker - Småoppgåver 1/js/main.js
@@ -1,3 +1,81 @@
 /*
  *Beginning of file
  */
+
+/*
+ * Making a fuction so i can decide i i want to console.log() the for loop or not,'
+ * and being able to send send the values bakc to the website
+ * Making a foor loop to console.log() the numbers from  1 to 100.
+ */
+
+const to_hundred = () => {
+    for (let i = 1; i < 101;
+        i++) {
+            console.log(i);
+        };
+};
+
+/*
+ * Making a fuction so i can decide i i want to console.log() the for loop or not,'
+ * and being able to send send the values bakc to the website 
+ * Making a for loop to console.log() every other the numbers from 1 to 100.
+ */
+
+const every_other = () => {
+    for (let i = 1; i < 101;
+        i += 2) {
+            console.log(i);
+        };
+};
+
+/*
+ * Making a fuction so i can decide i i want to console.log() the for loop or not,'
+ * and being able to send send the values bakc to the website
+ * Making a for loop to console.log() every number divdebal by 13.
+ */
+
+const divdebal_by_13 = () => {
+    for (let i = 1; i < 1001;
+    i ++) {
+        console.log(i/13);
+    };
+};
+//x = i 
+//array lise array
+
+/*
+ * Making a fuction so i can decide i i want to console.log() the for loop or not,'
+ * and being able to send send the values bakc to the website
+ * Making a for loop to console.log() every third number from 200 to 0-
+ */
+
+const every_third = () => {
+    for (let i = 200; i> 1;
+        i -= 3) {
+            console.log(i);
+        };
+};
+
+/*
+ * Making a fuction so i can decide i i want to console.log() the for loop or not,'
+ * and being able to send send the values bakc to the website.
+ * Making a for loop to console.log() every number divdebal by 3 and 5.
+ */
+
+const dvisjon
+for (let i = 1; i < 101;
+    i++) {
+    console.log(i)    
+}
+
+/*
+ * Making a fuction so i can decide i i want to console.log() the for loop or not,'
+ * and being able to send send the values bakc to the website
+ * Making a for loop to console.log() every number ither dividebal by 3 or 5.
+ */
+
+for (let i = 1; i < 101;
+    i ++) {
+        console.log(i);
+    };
+
