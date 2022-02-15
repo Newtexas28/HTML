81abb049cf23425bd21cc6653d32397090e7b853
Jon Håkon Lia <jh-lia@online.no>
2022-02-15 10:24:09 +0100

    Changes in Løkker

--- Løkker - Småoppgåver 1/js/main.js
+++ Løkker - Småoppgåver 1/js/main.js
@@ -3,79 +3,123 @@
  */
 
 /*
- * Making a fuction so i can decide i i want to console.log() the for loop or not,'
- * and being able to send send the values bakc to the website
- * Making a foor loop to console.log() the numbers from  1 to 100.
+ * Making a function so I can decide if I want to console.log() the for-loop or not,'
+ * and being able to send the values back to the website
+ * Making a for loop to console.log() the numbers from  1 to 100.
  */
 
+/*
+ * Using let instead of const when declearing the global variable numbers and index.
+ * Using const will not end an error message to the user but only the first,
+ * function will work. Only way to check this is in "utvikler verktøy".
+ * Ther you will be able to see where the error is organating from.
+ * In this case giving new values to numbers and index coudel resolv in an error
+ */
+
+let numbers = [];
+let index = 0;
+
 const to_hundred = () => {
-    for (let i = 1; i < 101;
-        i++) {
-            console.log(i);
-        };
+    numbers=[];
+    for (let i = 1; i < 101; i++)
+    {
+        numbers[i - 1] = i;
+    };
+    document.getElementById('to_hundred').innerHTML = numbers;
 };
 
 /*
- * Making a fuction so i can decide i i want to console.log() the for loop or not,'
- * and being able to send send the values bakc to the website 
+ * Making a function so I can decide if I want to console.log() the for-loop or not,'
+ * and being able to send the values back to the website
  * Making a for loop to console.log() every other the numbers from 1 to 100.
  */
 
 const every_other = () => {
-    for (let i = 1; i < 101;
-        i += 2) {
-            console.log(i);
-        };
+    numbers = [];
+    index = 0;
+    for (let i = 1; i < 101; i += 2)
+    {
+        numbers[index] = i
+        index++
+    };
+    document.getElementById('every_other').innerHTML = numbers;
 };
 
 /*
- * Making a fuction so i can decide i i want to console.log() the for loop or not,'
- * and being able to send send the values bakc to the website
- * Making a for loop to console.log() every number divdebal by 13.
+ * Making a function so I can decide if I want to console.log() the for-loop or not,'
+ * and being able to send the values back to the website
+ * Making a for loop to console.log() every number dividable by 13.
+ * Starting on 13 and jump with 13, because each number of the form,
+ * (13 * n) where n is an integer. 
  */
 
-const divdebal_by_13 = () => {
-    for (let i = 1; i < 1001;
-    i ++) {
-        console.log(i/13);
+const dividable_by_13 = () => {
+    numbers = [];
+    index = 0;
+    for (let i = 13; i < 1001; i += 13)
+    {
+        numbers[index] = i;
+        index++;
     };
+    document.getElementById('dividable_by_13').innerHTML = numbers;
 };
-//x = i 
-//array lise array
 
 /*
- * Making a fuction so i can decide i i want to console.log() the for loop or not,'
- * and being able to send send the values bakc to the website
+ * Making a function so I can decide if I want to console.log() the for-loop or not,'
+ * and being able to send the values back to the website
  * Making a for loop to console.log() every third number from 200 to 0-
  */
 
-const every_third = () => {
-    for (let i = 200; i> 1;
-        i -= 3) {
-            console.log(i);
-        };
+const down_every_third = () => {
+    numbers = [];
+    index = 0;
+    for (let i = 200; i > 0; i -= 3)
+    {
+        numbers[index] = i;
+        index++;
+    };
+    document.getElementById('down_every_third').innerHTML = numbers;
 };
 
 /*
- * Making a fuction so i can decide i i want to console.log() the for loop or not,'
- * and being able to send send the values bakc to the website.
- * Making a for loop to console.log() every number divdebal by 3 and 5.
+ * Making a function so I can decide if I want to console.log() the for-loop or not,'
+ * and being able to send the values back to the website
+ * Making a for loop to console.log() every number dividable by 3 and 5.
+ * I am checking if the number is dividable by 3 and 5 by checking the remainder.
  */
 
-const dvisjon
-for (let i = 1; i < 101;
-    i++) {
-    console.log(i)    
-}
-
+const division_by_3_and_5 = () => {
+    numbers = [];
+    index = 0;
+    for (let i = 1; i < 101; i++)
+    {
+        const n = i % 3
+        const m = i % 5
+        if (n === 0 && m === 0) {
+            numbers[index] = i;
+            index++;
+        }    
+    }
+    document.getElementById('division_by_3_and_5').innerHTML = numbers;
+};
 /*
- * Making a fuction so i can decide i i want to console.log() the for loop or not,'
- * and being able to send send the values bakc to the website
- * Making a for loop to console.log() every number ither dividebal by 3 or 5.
+ * Making a function so I can decide if I want to console.log() the for-loop or not,'
+ * and being able to send the values back to the website
+ * Making a for loop to console.log() every number dividable by 3 or 5.
+ * I am checking if the number is dividable by 3 or 5 by checking the remainder.
  */
 
-for (let i = 1; i < 101;
-    i ++) {
-        console.log(i);
-    };
-
+const division_by_3_or_5 = () => {
+    numbers = [];
+    index = 0;
+    for (let i = 1; i < 101; i++)
+    {
+        const n = i % 3
+        const m = i % 5
+        if (n === 0 || m === 0) {
+            numbers[index] = i;
+            index++;
+        }    
+    }
+    document.getElementById('division_by_3_or_5').innerHTML = numbers;
+};
