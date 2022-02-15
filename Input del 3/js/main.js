50f4e2bfdbc460910e22667181acfc8680584ab1
Jon Håkon Lia <jh-lia@online.no>
2022-02-15 10:24:28 +0100

    Input del 3

--- Input del 3/js/main.js
+++ Input del 3/js/main.js
@@ -1,6 +1,50 @@
 /*
  * Beging of main.js
  */
+    
+/*
+ * Making a function plus to add the two input values together,
+ * add returning the value.
+ */
+
+const plus = () => {
+    const input1 = parseInt(document.getElementById('number1').value);
+    const input2 = parseInt(document.getElementById('number2').value);
+    document.getElementById('resultat').innerHTML = input1 + input2; 
+};
+
+/*
+ * Making a function minus to subtract the two input values from each other,
+ * in this case subtracting input2 from input1 and returing the value.
+ */
+
+const minus = () => {
+    const input1 = parseInt(document.getElementById('number1').value);
+    const input2 = parseInt(document.getElementById('number2').value);
+    document.getElementById('resultat').innerHTML = input1 -input2
+};
+
+/*
+ * Making a function multiplication to multipli input1 and input2
+ * the returning the value.  
+ */
+
+const multiplication = () => {
+    const input1 = parseInt(document.getElementById('number1').value);
+    const input2 = parseInt(document.getElementById('number2').value);
+    document.getElementById('resultat').innerHTML = input1 * input2;
+};
+
+/*
+ * Making a function to divide the numbers from input1 with the numbers from input2,
+ * then returning the value.
+ */
+
+const division = () => {
+    const input1 = parseInt(document.getElementById('number1').value);
+    const input2 = parseInt(document.getElementById('number2').value);
+    document.getElementById('resultat').innerHTML = input1 / input2;
+};
 
 /*
  * Taking user inputs from the user on the websit and declaring it in the variabels,
@@ -10,44 +54,23 @@
  * Using parseInt() to convert the string thet comes from the website into numbers.
  * 
  */
-
-const output = () => {
+/*
+const output =() =>{
     const input1 = parseInt(document.getElementById('number1').value);
     const input2 = parseInt(document.getElementById('number2').value);
-    
-    /*
-    * Making a function plus to add the two input values together,
-    * add returning the value.
-    */
-
-    const plus = (input1, input2) => {
-        return input1 + input2; 
-    };
-
-    /*
-     * Making a function minus to subtract the two input values from each other,
-     * in this case subtracting input2 from input1 and returing the value.
-     */
-
-    const minus = (input1, input2) => {
-        return input1 - input2;
-    };
-
-    /*
-     * Making a function multiplication to multipli input1 and input2
-     * the returning the value.  
-     */
-    const multiplication = (input1, input2) => {
-        return input1 * input2;
-    };
-
-    /*
-     * Making a function to divide the numbers from input1 with the numbers from input2,
-     * then returning the value.
-     */
-
-    const divisijon = (input1, input2) => {
-        return input1/input2;
-    };   
-
+    const p = plus(input1, input2);
+    const mi = minus(input1, input2);
+    const mu = multiplication(input1, input2);
+    const d = division(input1, input2);
+    let answer = 0;
+    if (p = plus) {
+        
+    }
+    else if (condition) {
+        
+    } else {
+        
+    }
+    document.getElementById('output').innerHTML = answer
 };
+*/
