aabbe0ddc4f81c8e3af80d080ad4f72dec7693e1
Newtexas <89256338+Newtexas28@users.noreply.github.com>
2022-02-28 19:15:35 +0100

    change

--- Funksjoner del 4/js/main.js
+++ Funksjoner del 4/js/main.js
@@ -7,31 +7,39 @@
  * 
  */
 
-/*
- * Begining of main.js
- */
-
-/*
- *
- */
-
 const initialValue = 0;
-const array = [2, 3, 4, 2, 4, 44, 54];
+
+
 
 const average = array.reduce(
     (previousValue, currentValue) => previousValue + currentValue,
     initialValue)/array.length
+
 /*
- * Making a console.log() to check if the awner id correct.
- */
+ * Making a function sum with the input values x, y, z, n.
+ * Then returning the vaule of the first 4 value elmements in the array
+ * and dividing it on the length of the array
+ */ 
 
 
-const sum = (x, y, z, n) => {
-    return (x + y + z + n)/array.length;
+const sum = (x, y, z, n, array) => {
+    return (x + y + z + n) / array.length;
 };
+
+/*
+ * Making a console.log() to check if the anwser is correct.
+ */
 
 console.log(average);
 console.log(sum(...array));
+
+/*
+ * making a function to retrun the value of average, 
+ * when cliking the button on the website. 
+ */
 const output = () => {
-    document.getElementById('average').innerHTML = average;
+    let new_array = parseInt(document.getElementById('input1').value);
+    const array = new_array.split(" ");
+
+    document.getElementById('average').innerHTML = toString(sum);
 };
