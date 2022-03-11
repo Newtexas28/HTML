a086145bbc897edac9183929123b59fda1b26bf7
Jon Håkon Lia <jh-lia@online.no>
2022-03-11 13:25:18 +0100

    changes to code to code

--- Pizzameny/js/main.js
+++ Pizzameny/js/main.js
@@ -1,6 +1,28 @@
 /*
  * Beginning of main.js 
+ * 
  * Author: Jon Håkon Lia <jonlia28@innlandetfylke.no>
+ */  
+
+/*
+ * Make a function called output()
+ *
  */ 
- 
-const Pizza_ingrediens = [];
+
+const output = () => {
+    const Pizzaingrediens = ["ost", "Tommatsaus"];
+
+    const addtopping1 = document.getElementById('add1');
+    const addtopping2 = document.getElementById('add2');
+    const remove1 = document.getElementById('remove1');
+    const remove2 = document.getElementById('remove2');
+
+    const a1 = Pizzaingrediens.push(addtopping1);
+    Pizzaingrediens.push(addtopping2);
+    Pizzaingrediens.pop(remove1); 
+    Pizzaingrediens.pop(remove2);
+
+    const ingridiensene = "Ingridiensene er: " + addtopping1;
+
+    document.getElementById('topping').innerHTML = ingridiensene;
+};
